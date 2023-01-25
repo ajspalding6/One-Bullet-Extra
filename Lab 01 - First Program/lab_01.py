b9ccbbd976466e7c424ccf67ff4e84bf297e09e0
Aaron <as94901@email.vccs.edu>
2023-01-25 10:12:18 -0500

    Finished lab

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,8 @@
-print("hello, this is to test the history")
+name = "Aaron Spalding"
+print("Hello World")
+print("This is a test program to compute outputting text")
+print("\tThis line is tabbed")
+print("\"This line is in quotes\"")
+print("This line has a break \nin the middle of it")
+print("\nMy name is " + name)
+print("\nThe above line was assigning a string variable and outputting it with text")
